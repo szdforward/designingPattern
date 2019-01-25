71db4c4b79336da983ef3dc0fb229353ceaddb4f
forward22222 <szdforward@163.com>
2019-01-25 20:41:05 +0800

    观察者模式

---
+++ src/main/java/_8observer/BlogUser.java
@@ -0,0 +1,19 @@
+package _9flyWeight;
+
+import _8observer.Article;
+
+import java.util.Observable;
+
+/**
+ * Created by Administrator on 下午 2:57.
+ * 被观察者  实现Observable类
+ */
+public class BlogUser extends Observable {
+    //发表了博客
+    public void publicBlog(){
+        System.out.println("博士发博客");
+        this.setChanged();
+        Article article = new Article("如何成为大牛？", "天天学习，不断进步，对未知的知识充满渴求，量变引起质变！");
+        this.notifyObservers(article);
+    }
+}
